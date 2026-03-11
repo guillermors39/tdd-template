ec2e0e90708656c35777046329158f6b25b05a24
Guillermo Rolon Soto <g.rolon@muchosol.com>
2026-03-11 19:03:19 +0100

    feat(mysql): preparing just in case

--- tests/e2e/jest.e2e.setup.ts
+++ tests/e2e/jest.e2e.setup.ts
@@ -2,15 +2,11 @@ import { bootstrap } from '../../src/bootstrap';
 import nock from 'nock';
 import { Server } from 'http';
 import { Application } from 'express';
-import { StartedMySqlContainer, MySqlContainer } from '@testcontainers/mysql';
 
-let container: StartedMySqlContainer;
 let server: Server;
 let app: Application;
 
 beforeAll(async () => {
-    container = await new MySqlContainer('mysql:8').withExposedPorts(5432).start();
-
     app = bootstrap();
     server = app.listen(3001);
 
@@ -26,7 +22,6 @@ afterAll(async () => {
     nock.cleanAll();
     nock.restore();
     server.close();
-    await container.stop();
 }, 60000);
 
 export { app };
